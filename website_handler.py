cdc7de1ffcaa2fa45a98e2c5b339d4348e999a99
Saad Golandaz <golandazsaad@gmail.com>
2024-06-17 18:36:03 +0100

    Connected bio_test.html to website handler.py and changed html for html file to adapt for flask

--- website_handler.py
+++ website_handler.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
+from IRT_Algorithm import IRT
 
-app = Flask(__name__, template_folder='frontend')
+app = Flask(__name__, template_folder='frontend', static_folder='frontend')
 
 
 demo_question = "This is a demo question"
@@ -10,7 +11,7 @@ demo_question = "This is a demo question"
 def show_results():
 
     # Render the template from the 'websites' folder
-    return render_template('saad_test.html', question=demo_question)
+    return render_template('bio test.html', question=demo_question)
 
 
 if __name__ == '__main__':
